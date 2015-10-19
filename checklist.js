af1a0d9dae2c45bf60911c7d01b2625ba0419967
barbpe01 <barbpe01@users.noreply.github.com>
2015-10-19 16:20:11 -0500

    Update checklist.js

--- checklist.js
+++ checklist.js
@@ -1,6 +1,9 @@
 addTodo = function {
-  var newIn;
+  newLi = document.createElement("li");
   newIn = document.createElement("input");
   newIn.type="checkbox";
+  contents = document.createTextNode(myPriority.value);
+  newIn.appendChild(contents);
+  document.body.appendChild(newIn);
   newIn.className= myPriority.value;
 }
